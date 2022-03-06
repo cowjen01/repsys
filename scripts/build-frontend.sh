c156be080244f930a35a831490609c99e721539e
Jan Safarik <cowjen01@gmail.com>
2022-03-06 12:48:10 +0100

    improve fe build script, increase version

--- scripts/build-frontend.sh
+++ scripts/build-frontend.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cd ./frontend
-npm install
-npm run build
+if [ ! -d "./repsys/web/build" ]; then
+  cd ./frontend
+  npm install
+  npm run build
 
-cd ..
-mkdir -p ./repsys/web
-cp -r ./frontend/build ./repsys/web/build
+  cd ..
+  mkdir -p ./repsys/web
+  cp -r ./frontend/build ./repsys/web/build
+fi
