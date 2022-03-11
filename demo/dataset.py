f0997d4336ab7f4977bbe88927ee4a51ad8c867c
Jan Safarik <cowjen01@gmail.com>
2022-03-11 01:05:03 +0100

    remove nmf from demo dataset class

--- demo/dataset.py
+++ demo/dataset.py
@@ -1,22 +1,12 @@
 import pandas as pd
-from scipy.sparse import csr_matrix
-from sklearn.decomposition import NMF
 
 import repsys.dtypes as dtypes
 from repsys.dataset import Dataset
 
 
 class MovieLens(Dataset):
-    def __init__(self):
-        self.nmf = NMF(n_components=10, init="nndsvd", max_iter=100, random_state=0, verbose=2)
-
     def name(self):
         return "ml20m"
-
-    def compute_embeddings(self, X: csr_matrix):
-        W = self.nmf.fit_transform(X)
-        H = self.nmf.components_
-        return W, H.T
 
     def item_cols(self):
         return {
