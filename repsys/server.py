f0dc22e249d1692e41db4a4f03d4a7e17f8e38d3
Jan Safarik <cowjen01@gmail.com>
2022-06-16 20:27:06 +0200

    remove localhost from sanic

--- repsys/server.py
+++ repsys/server.py
@@ -366,4 +366,4 @@ def run_server(
 ) -> None:
     app = create_app(models, dataset, dataset_eval, model_eval)
     app.config.FALLBACK_ERROR_FORMAT = "json"
-    app.run(host="localhost", port=config.server_port, debug=False, access_log=False)
+    app.run(port=config.server_port, debug=False, access_log=False)
